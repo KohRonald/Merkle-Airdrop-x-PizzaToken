5edfb579be99d23cced55e266f883ccf5872153b
RonaldKoh <ronaldkohzx@gmail.com>
2025-12-15 16:43:35 +0800

    Amend interact script

--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -5,6 +5,11 @@ import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
 
+/**
+ * This contract takes the Signature of a signed message from address (A) to allow address (B) to call airdrop claim on their behalf.
+ * Airdrop claim address validation handled by MerkleAirdrop contract.
+ * If address (B) trys to claim for address (C) and address (C) is not eligible for aidrop, then no claim will occur.
+ */
 contract ClaimAirdrop is Script {
     error __ClaimAirdropScript_InvalidSignatureLength();
 
@@ -13,8 +18,8 @@ contract ClaimAirdrop is Script {
 
     //This proofs were generated and written in output.json that is under the user's address
     //The Proof are for the second address, which is the claiming address
-    Bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
-    Bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] proof = [PROOF_ONE, PROOF_TWO];
     bytes private SIGNATURE =
         hex"fbd2270e6f23fb5fe9248480c0f4be8a4e9bd77c3ad0b1333cc60b5debc511602a2a06c24085d8d7c038bad84edc53664c8ce0346caeaa3570afec0e61144dc11c";
@@ -52,7 +57,7 @@ contract ClaimAirdrop is Script {
      * @return s The svalue of the Signature
      * @dev uint8 == byte 1
      */
-    function splitSignature(bytes sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+    function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
         //validate that the signaure is of correct length
         if (sig.length != 65) {
             revert __ClaimAirdropScript_InvalidSignatureLength();
@@ -64,6 +69,16 @@ contract ClaimAirdrop is Script {
         }
     }
 
+    /**
+     * @dev Call the function as the non-claiming address, to claim on behalf of the claimer. Claimer will receive the airdrop, non-claiming address will pay the gas fee. Test is done on CLI:
+     *  - forge script script/Interact.s.sol:ClaimAirdrop --rpc-url http://localhost:8545 --private-key 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d --broadcast
+     * @dev To verify the airdrop recived by claimer:
+     *  1. cast call <token contract> "balanceOf(address)" <receiving account public address>
+     *      a. cast call 0x5FbDB2315678afecb367f032d93F642f64180aa3 "balanceOf(address)" 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+     *
+     *  2. cast the return bytes to dec
+     *      a. cast --to-dec 0x0000000000000000000000000000000000000000000000015af1d78b58c40000
+     */
     function run() external {
         address monstRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
         claimAirdrop(monstRecentlyDeployed);
